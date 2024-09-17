1c5d031941781bd87ae5b9aabd96786986bffde5
NithinReddy738 <nnimmala@uncc.edu>
2024-09-17 19:54:22 +0000

    Completed Movie Script Analysis Assignment

--- src/main/java/com/movie/script/analysis/UniqueWordsMapper.java
+++ src/main/java/com/movie/script/analysis/UniqueWordsMapper.java
@@ -14,6 +14,24 @@ public class UniqueWordsMapper extends Mapper<Object, Text, Text, Text> {
 
     @Override
     public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        String[] parts = line.split(":");
+        if(parts.length==2){
+            character.set(parts[0].trim());
+            String dialogue = parts[1].trim();
+            StringTokenizer tokenizer = new StringTokenizer(dialogue);
+            while(tokenizer.hasMoreTokens()){
+                String token = tokenizer.nextToken().replaceAll("[^a-zA-Z]", "");
+                HashSet<String> wordsSpoken = new HashSet<String>();
+                if(!token.isEmpty() ){
+                    wordsSpoken.add(token);
+                    if(!wordsSpoken.contains(token)){
+                        word.set(token);
+                        context.write(character, word);
+                    }
 
+                }
+            }
+        }
     }
 }
